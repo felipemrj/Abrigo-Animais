776b831fbc1243823ea119238c2eaa1826cdd392
felipemrj <157051881+felipemrj@users.noreply.github.com>
2025-10-07 17:03:24 -0300

    feat: implementada opção para encerrar o programa, agora o menu principal roda em um loop até que o usuário deseje encerrar

--- src/main/Main.java
+++ src/main/Main.java
@@ -14,32 +14,37 @@ public class Main {
         DeletePet dp = new DeletePet();
         SearchPet sp = new SearchPet();
 
+        boolean runProgram = true;
 
-        MainMenu ui = new MainMenu();
-        int selectedOption = ui.Menu();
+        do {
+            MainMenu ui = new MainMenu();
+            int selectedOption = ui.Menu();
 
-        switch (selectedOption) {
-            case 1:
-                rp.registerPet();
-                break;
-            case 2:
-                mp.modifyPet();
-                break;
-            case 3:
-                dp.deletePet();
-                break;
-            case 4:
-                System.out.println("Under development");
-                break;
-            case 5:
-                sp.searchPet();
-                break;
-            case 6:
-                System.out.println("Under development");
-                return;
-            default:
-                System.out.println("Invalid");
-                break;
-        }
+            switch (selectedOption) {
+                case 1:
+                    rp.registerPet();
+                    break;
+                case 2:
+                    mp.modifyPet();
+                    break;
+                case 3:
+                    dp.deletePet();
+                    break;
+                case 4:
+                    System.out.println("Under development");
+                    break;
+                case 5:
+                    sp.searchPet();
+                    break;
+                case 6:
+                    runProgram = false;
+                    System.out.println("Encerrando o sistema.");
+                    break;
+                default:
+                    System.out.println("Invalid");
+                    break;
+            }
+        } while (runProgram);
+
     }
 }
